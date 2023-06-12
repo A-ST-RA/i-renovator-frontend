f1fc2a7fb09a809a64e833395b35c4b6b8ebd7a6
Владислав Бахмацкий <v.bakhmatsky@webpractik.ru>
2023-06-12 16:09:41 +0300

    feat(top authors list): сделал кнопки в списки авторов

--- src/components/widgets/TopAuthorsList/index.tsx
+++ src/components/widgets/TopAuthorsList/index.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 import React from 'react';
 
 import TopAuthorCard from '@/components/entities/top-author-card';
@@ -17,37 +17,39 @@ interface ITopAuthorsListProps {
 
 export default function TopAuthorsList({ authors }: ITopAuthorsListProps) {
     const splideOptions = {
-        arrows: false,
+        // arrows: false,
         type: 'loop',
+        rewind: true,
         gap: '40px',
         drag: true,
         pagination: false,
         perMove: 1,
-        classes: {
-            arrows: 'splide__arrows',
-            arrow: 'splide__arrow',
-            prev: 'splide__arrow--prev your-class-prev',
-            next: 'splide__arrow--next your-class-next',
+        updateOnMove: false,
+        perPage: 2,
+        autoScroll: {
+            speed: 2,
         },
     };
 
     return (
         <div className={cn.topAuthorsList}>
-            <Splide options={splideOptions}>
-                {authors.map((el, index) => (
-                    <SplideSlide key={el.id}>
-                        <TopAuthorCard rankPlace={index + 1} author={el} />
-                    </SplideSlide>
-                ))}
+            <Splide hasTrack={false} options={splideOptions}>
+                <SplideTrack>
+                    {authors.map((el, index) => (
+                        <SplideSlide key={el.id}>
+                            <TopAuthorCard rankPlace={index + 1} author={el} />
+                        </SplideSlide>
+                    ))}
+                </SplideTrack>
+                <div className="splide__arrows">
+                    <button type="button" className="splide__arrow splide__arrow--prev">
+                        Prev
+                    </button>
+                    <button type="button" className="splide__arrow splide__arrow--next">
+                        Next
+                    </button>
+                </div>
             </Splide>
-            <div className="splide__arrows">
-                <button type="button" className="splide__arrow splide__arrow--prev">
-                    Prev
-                </button>
-                <button type="button" className="splide__arrow splide__arrow--next">
-                    Next
-                </button>
-            </div>
         </div>
     );
 }
